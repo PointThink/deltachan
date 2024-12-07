0570069870f92f57916b0a63cea8eac2ed7de75b
PointThink <69512713+PointThink@users.noreply.github.com>
2024-12-07 21:11:00 +0100

    Moved theme selector to settings for users with js enabled

--- src/settings.php
+++ src/settings.php
@@ -5,6 +5,7 @@ include_once "internal/ui.php";
 if (count($_POST) > 0)
 {
     $_SESSION["user_css"] = $_POST["user_css"];
+    setcookie("theme", $_POST["theme"], time() + 60*60*24*30, "/");
 }
 ?>
 
@@ -21,8 +22,13 @@ if (count($_POST) > 0)
         <h1 class=title>Settings</h1>
         <div class=post_form>
         <?php
+			$themes = scandir(__DIR__ . "/internal/styles/");
+			array_push($themes, "default");
+            $themes = array_diff($themes, array("res", "..", "."));
+
             (new PostForm("", "POST"))
                 ->add_text_area("User CSS", "user_css", $_SESSION["user_css"])
+                ->add_dropdown("Theme", "theme", $themes, $_COOKIE["theme"])
                 ->finalize();
         ?>
         </div>
