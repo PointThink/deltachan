14bde0d7a1700b1a45fa8cb7efde2eaf7cc50998
PointThink <69512713+PointThink@users.noreply.github.com>
2024-12-28 17:34:50 +0100

    Updated version in footer

--- src/footer.php
+++ src/footer.php
@@ -1,6 +1,6 @@
 <hr>
 <div id=footer>
-	<p><a href=https://github.com/pointThink/deltachan>DeltaChan</a> v1.4</p>
+	<p><a href=https://github.com/pointThink/deltachan>DeltaChan</a> v1.5</p>
 	<a href="http://www.geoplugin.com/geolocation/" target="_new">IP Geolocation</a> by <a href="http://www.geoplugin.com/" target="_new">geoPlugin</a>
 </div>
 <br>
