6e9a360c3c59f87749d381e21686e0924bfd2c3c
PointThink <69512713+PointThink@users.noreply.github.com>
2025-01-02 20:34:51 +0100

    Removed make.sh script since it is no longer necessary

--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cp -r src site-build
-rm site-build/internal/config.php
-rm site-build/internal/chaninfo.json
-rm -rf site-build/uploads
-rm -rf site-build/def
-touch site-build/first_run
