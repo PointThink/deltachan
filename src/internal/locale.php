f06b8288bd7344b0a1ef62815557d51a66f5436c
PointThink <69512713+PointThink@users.noreply.github.com>
2025-02-02 11:54:00 +0100

    Locale now falls back to english if string is not found

--- src/internal/locale.php
+++ src/internal/locale.php
@@ -3,17 +3,22 @@ include_once "chaninfo.php";
 
 $locale_name = chan_info_read()->locale;
 
-function load_locale()
+function load_locale($name)
 {
-    global $locale_name;
-    $jsonContent = file_get_contents(__DIR__ . "/locales/$locale_name.json");
+    $jsonContent = file_get_contents(__DIR__ . "/locales/$name.json");
     return json_decode($jsonContent, true);
 }
 
 function localize($name)
 {
     global $locale;
-    return $locale[$name];
+    global $fallback_locale;
+    
+    if (isset($locale[$name]))
+        return $locale[$name];
+    else
+        return $fallback_locale[$name];
 }
 
-$locale = load_locale();
+$fallback_locale = load_locale("en");
+$locale = load_locale($locale_name);
