955b95c24ed274a079fb82ca9eac3ac3ccf4a96b
PointThink <69512713+PointThink@users.noreply.github.com>
2025-02-02 17:10:43 +0100

    Added post rate limiting

--- src/internal/actions/staff/chan_setup.php
+++ src/internal/actions/staff/chan_setup.php
@@ -16,16 +16,17 @@ if (count($_POST) > 0)
     $chan_info->welcome = $_POST["welcome"];
 	$chan_info->faq = $_POST["faq"];
 	$chan_info->default_theme = $_POST["default_theme"];
-	$chan_info->show_ban_list = $_POST["show_ban_list"];
+	$chan_info->show_ban_list = isset($_POST["show_ban_list"]);
 	$chan_info->locale = $_POST["locale"];
+	$chan_info->rate_limit_max_threads = $_POST["rate_limit_max_threads"];
+	$chan_info->rate_limit_range = $_POST["rate_limit_range"];
+	$chan_info->rate_limiting_enabled = isset($_POST["rate_limiting_enabled"]);
 
 	$key = file_get_contents($deltachan_config["crypt_key_path"]);
 	$chan_info->turnslite_site_key = $_POST["turnslite_site_key"];
 	$chan_info->turnslite_secret_key = openssl_encrypt($_POST["turnslite_secret_key"], "aes-256-ecb", $key);
 
     chan_info_write($chan_info);
-
-	header("Location: /admin.php");
 }
 ?>
 
@@ -57,6 +58,9 @@ if (count($_POST) > 0)
 				->add_checkbox("Public ban list enabled", "show_ban_list", $chan_info->show_ban_list)
 				->add_text_field("Default theme", "default_theme", $chan_info->default_theme)
 				->add_dropdown("Locale", "locale", array("en", "pl", "ru"), $chan_info->locale)
+				->add_checkbox("Post rate limiting enabled", "rate_limiting_enabled", $chan_info->rate_limiting_enabled)
+				->add_number("Rate limit range (seconds)", "rate_limit_range", $chan_info->rate_limit_range)
+				->add_number("Max posts in range", "rate_limit_max_threads", $chan_info->rate_limit_max_threads)
 				->add_text_field("Turnslite site key", "turnslite_site_key", $chan_info->turnslite_site_key)
 				->add_text_field("Turnslite secret key", "turnslite_secret_key", openssl_decrypt($chan_info->turnslite_secret_key, "aes-256-ecb", $key))
 				->finalize();
